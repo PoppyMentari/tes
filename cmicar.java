c02f2dca7fcd38b4484ad9e4b8a445de97258950
PoppyMentari <mentari_poppy@yahoo.com>
2015-06-08 16:27:54 +0700

    Create cmicar.java

---
+++ cmicar.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class CmiCar {
+public CmiCar () {
+int CmiEngine;
+int CmiObjectCensor;
+int CmiLightCensor;
+return new CmiCar ((CmiEngine+CmiObjectCensor+CmiLightCensor)/3);
+}
+}
